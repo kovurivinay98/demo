b4432514115a4f9258c9137956cf7e00dc96d747
K.Vinay Reddy <vinayreddykovuri@gmail.com>
2025-11-09 15:53:44 +0530

    vinay commited

--- 02-demo.sh
+++ 02-demo.sh
@@ -1,8 +1,21 @@
+#!/bin/bash
 userid=$(id -u)
 if [ $userid -eq 0 ]
  then
    echo "Your are root user"
     dnf list installed nginx
+     if [ $? -ne 0 ]
+      then
+        dnf install nginx -y
+          if [ $? -eq 0 ]
+             then 
+                echo "Installed successfully"
+             else
+                echo "Not Installed succesfully"
+            fi
+       else
+        echo "nginx already installed"
+    fi
  else
    echo "You are not root user"
 fi
